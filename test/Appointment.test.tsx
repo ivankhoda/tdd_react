a50dcf6b1171b72fc5ef69bedd34d13051c6cbc4
Ivan Khoda <ivankhoda@gmail.com>
2021-12-21 21:47:31 +0300

    Small refactoring

--- test/Appointment.test.tsx
+++ test/Appointment.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "react-dom";
-import { Appointment } from "../src/Appointment";
+import ReactDOM, { render } from "react-dom";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 describe("Appointment", () => {
   let container;
   let customer;
@@ -18,5 +18,48 @@ describe("Appointment", () => {
 
     render(<Appointment customer={customer} />, container);
     expect(container.textContent).toMatch("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+  const today = new Date();
+  const appointments = [
+    {
+      startsAt: today.setHours(12, 0),
+      customer: { firstName: "Ashley" },
+    },
+    {
+      startsAt: today.setHours(13, 0),
+      customer: { firstName: "Jordan" },
+    },
+  ];
+  const render = (component) => ReactDOM.render(component, container);
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+  it("renders multiple appointments in an ol element", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector("ol")).not.toBeNull();
+    expect(container.querySelector("ol").children).toHaveLength(2);
+  });
+  it("renders each appointment in an li", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll("li")).toHaveLength(2);
+    expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
+    expect(container.querySelectorAll("li")[1].textContent).toEqual("13:00");
+  });
+  it("initially shows a message saying there are no appointment today", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch("There are no appointments scheduled for today.");
+  });
+  it("selects the first appointment by default", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.textContent).toMatch("Ashley");
   });
 });
