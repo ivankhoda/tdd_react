2f6363cccf6d987300373773138efba7ab6634ae
Ivan Khoda <ivankhoda@gmail.com>
2021-12-24 14:17:56 +0300

    More exercises

--- src/CustomerForm.tsx
+++ src/CustomerForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-export const CustomerForm = () => (
+export const CustomerForm = ({ firstName }) => (
   <form id="customer">
-    <input type="text" name="firstName" />
+    <input type="text" name="firstName" readOnly value={firstName} />{" "}
   </form>
 );
--- test/CustomerForm.test.tsx
+++ test/CustomerForm.test.tsx
@@ -4,18 +4,24 @@ import { createContainer } from "./domManipulators";
 describe("CustomerForm", () => {
   let render, container;
   const form = (id) => container.querySelector(`form[id="${id}"]`);
+  const firstNameField = () => form("customer").elements.firstName;
   beforeEach(() => {
     ({ render, container } = createContainer());
   });
   it("renders a form", () => {
-    render(<CustomerForm />);
+    render(<CustomerForm firstName={undefined} />);
     expect(form("customer")).not.toBeNull();
   });
   it("renders the first name field as a text box", () => {
-    render(<CustomerForm />);
-    const field = form("customer").elements.firstName;
-    expect(field).not.toBeNull();
-    expect(field.tagName).toEqual("INPUT");
-    expect(field.type).toEqual("text");
+    render(<CustomerForm firstName={undefined} />);
+
+    expect(firstNameField()).not.toBeNull();
+    expect(firstNameField().tagName).toEqual("INPUT");
+    expect(firstNameField().type).toEqual("text");
+  });
+  it("includes the existing value for the first name", () => {
+    render(<CustomerForm firstName="Ashley" />);
+
+    expect(firstNameField().value).toEqual("Ashley");
   });
 });
